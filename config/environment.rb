4bbe7d9e6f5414e427db578474aa7ac7d4803cfc
rissem <rissem@gmail.com>
2012-12-01 12:58:58 -0800

    add gmail oauth2 credentials

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+GMAIL_CLIENT_ID = "693405860632.apps.googleusercontent.com"
+GMAIL_CLIENT_SECRET="uAzxHylgyqcTUVy-wb2dndrt"
+GMAIL_REDIRECT_URI="http://localhost/oauth2callback"
+
 # Initialize the rails application
 GmailPassbook::Application.initialize!
