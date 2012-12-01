d2ae8600fd9c002e9f3daac824ff201cc5d48b83
rissem <rissem@gmail.com>
2012-12-01 15:38:42 -0800

    fix require statement

--- app/controllers/inapptest_controller.rb
+++ app/controllers/inapptest_controller.rb
@@ -1,5 +1,4 @@
-require "lib/urban_airship"
+require "./lib/urban_airship"
 class InapptestController < ApplicationController
-	p "inapptest"
 	Urban_Airship.send_urban_airship_text_notification(params[:token], params[:text])
 end
