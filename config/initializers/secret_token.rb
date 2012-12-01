7f448d66f0f3f31e8591a14ff82cd17a2d93ab44
rissem <rissem@gmail.com>
2012-12-01 12:48:20 -0800

    barebones rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmailPassbook::Application.config.secret_token = '3f3ec2475eb782c47912ba4175f0d6f08ddc009c270f5c286cb8c4588be51d5770b9f054fba233905eb585128c0550de73e28f79fb3389b3d69eedf93d8d4eb3'
