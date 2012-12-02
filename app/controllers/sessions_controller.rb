ebb9d581bcb5a81b328e123ce950e231579e4216
rissem <rissem@gmail.com>
2012-12-02 01:50:18 -0800

    return email address after auth dance

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,8 @@
 class SessionsController < ApplicationController
   def create
     puts "STUFF WE GOT BACK #{auth_hash.to_json}"
-    
     @user = User.find_or_create_from_auth_hash(auth_hash)
-#    self.current_user = @user
-    redirect_to '/'
+    render :json => {email: @user.email}
   end
 
   protected
