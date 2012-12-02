7728d0b807b055ae2b321d645317f7a2a6856ab7
James Gill <jamesagill@gmail.com>
2012-12-02 00:22:27 -0800

    Initial scraping pdf method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,30 @@ class User < ActiveRecord::Base
       pass.save!
     end
   end
+
+
+  #Ticket number barcodes seem to be encoded like
+  #"\xC3\x8C1290412908123312321~\xC3\x8E"
+  #So we find indices based on these and then extract the number.
+  #starting bytes: \xC3\x8C [195, 140]
+  STARTING_BYTES = "\xC3\x8C"
+  #ending bytes: \x7E\xC3\x8E [126, 195, 142]
+  ENDING_BYTES = "\xC3\x8E"
+
+  def extractPdfInfo(pdfPath)
+    reader = PDF::Reader.new(pdfPath)
+    pageText = reader.page(1).text
+    #puts pageText
+    startIndex = pageText.chars.to_a.find_index(STARTING_BYTES)
+    puts "Found startIndex #{startIndex}"
+    startIndex += 1 #Because we don't want the unicode start char
+    endIndex = pageText.chars.to_a.find_index(ENDING_BYTES)
+    puts "Found endIndex #{endIndex}"
+    endIndex -= 2 #Because we don't want the unicode end char, or terminal '~'
+    ticketNum = pageText[startIndex..endIndex].to_i
+    puts "Found ticketNum #{ticketNum}"
+
+    return { 'ticket_number' => ticketNum }
+  end
+
 end
